96e0e232fe9f3965630a7470ee2685f062d2361c
schwamster <bastian.toepfer@bodoconsult.de>
2017-01-24 16:50:45 +0100

    added build number as version suffix

--- build-container.sh
+++ build-container.sh
@@ -3,4 +3,4 @@ set -e
 dotnet restore
 dotnet test test/health-check-middleware.test/project.json -xml $(pwd)/testresults/out.xml
 rm -rf $(pwd)/package
-dotnet pack src/health-check-middleware/project.json -c release -o $(pwd)/package
+dotnet pack src/health-check-middleware/project.json -c release -o $(pwd)/package --version-suffix=${BuildNumber}
