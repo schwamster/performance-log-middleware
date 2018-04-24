54ad020f8335fa220532ba19b40b042c8c3dc20d
schwamster <bastian.toepfer@gmail.com>
2018-04-24 08:20:24 +0200

    updated build script to point to csproj

--- build-container.sh
+++ build-container.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/performance-log-middleware.test/project.json -xml $(pwd)/testresults/out.xml
+dotnet test test/performance-log-middleware.test/performance-log-middelware.test.csproj -xml $(pwd)/testresults/out.xml
 rm -rf $(pwd)/package
-dotnet pack src/performance-log-middleware/project.json -c release -o $(pwd)/package --version-suffix=${BuildNumber}
+dotnet pack src/performance-log-middleware/performance-log-middelware.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
 mkdir $(pwd)/symbols
 cp $(pwd)/package/*.symbols.nupkg $(pwd)/symbols
 rm $(pwd)/package/*.symbols.nupkg
