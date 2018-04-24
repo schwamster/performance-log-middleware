1bc2670f3f937242e3c15b679d8e4dc6b677f176
schwamster <bastian.toepfer@gmail.com>
2018-04-24 08:37:20 +0200

    fixed typo

--- build-container.sh
+++ build-container.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/performance-log-middleware.test/performance-log-middelware.test.csproj --test-adapter-path:. --logger:xunit
+dotnet test test/performance-log-middleware.test/performance-log-middleware.test.csproj --test-adapter-path:. --logger:xunit
 rm -rf $(pwd)/package
-dotnet pack src/performance-log-middleware/performance-log-middelware.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
+dotnet pack src/performance-log-middleware/performance-log-middleware.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
 mkdir $(pwd)/symbols
 cp $(pwd)/package/*.symbols.nupkg $(pwd)/symbols
 rm $(pwd)/package/*.symbols.nupkg
