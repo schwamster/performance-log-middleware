1bedb92912b7b4d17a85484a05011de9dfd223bc
schwamster <bastian.toepfer@gmail.com>
2018-04-24 08:30:12 +0200

    updated tests

--- build-container.sh
+++ build-container.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 set -e
 dotnet restore
-dotnet test test/performance-log-middleware.test/performance-log-middelware.test.csproj -xml $(pwd)/testresults/out.xml
+dotnet test test/performance-log-middleware.test/performance-log-middelware.test.csproj --test-adapter-path:. --logger:xunit
 rm -rf $(pwd)/package
 dotnet pack src/performance-log-middleware/performance-log-middelware.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
 mkdir $(pwd)/symbols
