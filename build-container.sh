3a8f7489728357632f0e471275366720578a83de
schwamster <bastian.toepfer@gmail.com>
2018-04-24 10:16:57 +0200

    removed buildnumber from version

--- build-container.sh
+++ build-container.sh
@@ -3,7 +3,7 @@ set -e
 dotnet restore
 dotnet test test/performance-log-middleware.test/performance-log-middleware.test.csproj --test-adapter-path:. --logger:xunit
 rm -rf $(pwd)/package
-dotnet pack src/performance-log-middleware/performance-log-middleware.csproj -c release -o $(pwd)/package --version-suffix=${BuildNumber}
+dotnet pack src/performance-log-middleware/performance-log-middleware.csproj -c release -o $(pwd)/package
 mkdir $(pwd)/symbols
 cp $(pwd)/package/*.symbols.nupkg $(pwd)/symbols
 rm $(pwd)/package/*.symbols.nupkg
