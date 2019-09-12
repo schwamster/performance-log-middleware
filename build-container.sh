b95778f65c08917847720c827e4c362a95c52707
Bastian Töpfer <bastian.topfer@scania.com>
2019-09-12 11:11:49 +0200

    updated to new circle ci syntax

--- build-container.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-set -e
-dotnet restore
-dotnet test test/performance-log-middleware.test/performance-log-middleware.test.csproj --test-adapter-path:. --logger:xunit
-rm -rf $(pwd)/package
-dotnet pack src/performance-log-middleware/performance-log-middleware.csproj -c release -o $(pwd)/package
-mkdir $(pwd)/symbols
-cp $(pwd)/package/*.symbols.nupkg $(pwd)/symbols
-rm $(pwd)/package/*.symbols.nupkg
--- build.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f docker-compose-build.yml up
